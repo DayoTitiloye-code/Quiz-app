7ea992f0054cc59ffcb418b4ebdadc2191a2d402
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-09-12 12:52:10 +0100

    Added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css'
+import React, { useState } from 'react'
 
 function App() {
+  /*  used to determine the section 
+  of the quiz app users will be in
+  i.e. main menu, end of quiz, etc.*/
+  const [gameState, setGameState] = useState('menu')
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Quiz App</h1>
     </div>
   )
 }
