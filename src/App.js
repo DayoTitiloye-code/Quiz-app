72fd20a28ef0b59cf07d12f800e8f0a39d7a5a1d
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-09-12 13:22:25 +0100

    Implemented useContext hook in MainMenu.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css'
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import MainMenu from './components/MainMenu'
+import Quiz from './components/Quiz'
+import EndScreen from './components/EndScreen'
+import { QuizContext } from './helpers/Context'
 
 function App() {
   /*  used to determine the section 
@@ -10,6 +14,11 @@ function App() {
   return (
     <div className="App">
       <h1>Quiz App</h1>
+      <QuizContext.Provider value={{ gameState, setGameState }}>
+        {gameState === 'menu' && <MainMenu />}
+        {gameState === 'quiz' && <Quiz />}
+        {gameState === 'endScreen' && <EndScreen />}
+      </QuizContext.Provider>
     </div>
   )
 }
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { QuizContext } from '../helpers/Context'
 
 function MainMenu() {
-  return <div></div>
+  /*Used to gain access to state without having to use props */
+  const { gameState, setGameState } = useContext(QuizContext)
+
+  return (
+    <div className="Menu">
+      <button
+        onClick={() => {
+          setGameState('quiz')
+        }}
+      >
+        Start Quiz!
+      </button>
+    </div>
+  )
 }
 
 export default MainMenu
