7e283aaf1c31e4c792a3fbcac67a3b85eabf2bda
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-09-13 13:32:16 +0100

    Created array of questions for Quiz

---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Question = [
+  {
+    prompt: 'Which exercise works the triceps?',
+    optionA: 'Squats',
+    optionB: 'Bicep curls',
+    optionC: 'Bench press',
+    optionD: 'Pull ups',
+    answer: 'optionC',
+  },
+  {
+    prompt: 'Who is the fastest man alive?',
+    optionA: 'Elon Musk',
+    optionB: 'Usain Bolt',
+    optionC: 'Ryan Renolds',
+    optionD: 'Floyd Mayweather Jr',
+    answer: 'optionB',
+  },
+  {
+    prompt: 'How many Wimbledon championships has Serena Williams won?',
+    optionA: '7',
+    optionB: '12',
+    optionC: '3',
+    optionD: '4',
+    answer: 'optionA',
+  },
+]
