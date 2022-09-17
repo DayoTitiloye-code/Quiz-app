80e7d068390dd010c5d234b29f3511e1637692c2
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-09-17 20:37:34 +0100

    Finshed end screen (shows score now)

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { QuizContext } from '../helpers/Context'
+import '../App.css'
+import { Question } from '../helpers/Questions'
 
 function EndScreen() {
-  return <div>EndScreen</div>
+  const { score, setScore, setGameState } = useContext(QuizContext)
+
+  const restartQuiz = () => {
+    setScore(0)
+    setGameState('quiz')
+  }
+
+  const mainMenu = () => {
+    setScore(0)
+    setGameState('menu')
+  }
+  return (
+    <div className="EndScreen">
+      <h1>You have finished the quiz!</h1>
+      <h3>
+        Your score is: {score}/{Question.length}
+      </h3>
+      <button onClick={restartQuiz}>Restart Quiz</button>
+      <button onClick={mainMenu}>Back to main menu</button>
+    </div>
+  )
 }
 
 export default EndScreen
