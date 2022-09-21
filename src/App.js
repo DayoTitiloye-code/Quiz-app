0e1329e350b6da9f24d2239e6a87ed0a10f5e20d
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-09-21 11:01:34 +0100

    Changed title of application

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Quiz App</h1>
+      <h1>Sports Quiz</h1>
       <QuizContext.Provider
         value={{ gameState, setGameState, score, setScore }}
       >
